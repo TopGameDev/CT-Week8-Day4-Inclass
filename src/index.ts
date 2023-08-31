adab0b58c0a38535c4ca088bf1df1fdb97aee2ad
GameDev <gamemaster@GME.local>
2023-08-31 11:39:04 -0500

    Initial Setup with Webpack, TS, and live-server

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log("Hello my name is Trevon!!!");
+console.log("This is another logged line")
+
+
+function addNums(num1:number, num2:number):number{
+    return num1 + num2
+}
+
+console.log(addNums(10, 20))
