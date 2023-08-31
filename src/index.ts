1e1743547eae2ce4e00280a2e4d5f5eccab49ffa
GameDev <gamemaster@GME.local>
2023-08-31 14:24:47 -0500

    Add shape containers for the Component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import { Canvas } from "./Widget";
+import { Canvas, Component } from "./Widget";
 
 const canvas = new Canvas(document.body);
 console.log(canvas)
+
+const myComponent = new Component();
+console.log(myComponent);
+console.log(myComponent.shape);
+console.log(myComponent.shape.attributes)
